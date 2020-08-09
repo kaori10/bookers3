e1c1abde80b70fb02035bb07a1fd692444570b70
@kaori10 <0825pira@gmail.com>
2020-08-09 13:28:32 +0900

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,8 @@ class BooksController < ApplicationController
       before_action :authenticate_user!
   def index
   	@books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
@@ -15,18 +17,37 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	@book.user_id = current_user.id
-  	@book.save
-  	redirect_to book_path(@book)
+    @user = current_user
+    @books = Book.all
+  	if @book.save
+  	redirect_to book_path(@book), notice: 'successfully'
+    else
+      render :index
+    end
   end
 
   def edit
   	@book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path, alert: '不正なアクセスです'
+
+    end
   end
 
   def update
   	@book = Book.find(params[:id])
-  	@book.update(book_params)
-  	redirect_to book_path(@book)
+  	if @book.update(book_params)
+  	redirect_to book_path(@book), notice: 'successfully'
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+   @book = Book.find(params[:id])
+   @book.destroy
+   redirect_to books_path
+
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,34 @@
+
 class UsersController < ApplicationController
       before_action :authenticate_user!
+
   def index
-  	@user = current_user
+  	@users = User.all
+    @user = current_user
+    @book = Book.new
+
   end
 
   def show
      @user = User.find(params[:id])
+     
      @book = Book.new
   end
 
   def edit
   	@user = User.find(params[:id])
-
+    if @user != current_user
+      redirect_to user_path(current_user), alert: '不正なアクセスです'
+    end
   end
+
   def update
   	@user = User.find(params[:id])
-  	@user.update(user_params)
-  	redirect_to user_path(@user)
+  	if @user.update(user_params)
+  	redirect_to user_path(@user), notice: 'successfully'
+  else
+    render :edit
+  end
   end
 
   private
